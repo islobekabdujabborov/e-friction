769a8eec946114b8fe10e9bd5d8326c0201b7c7d
islobekabdujabborov <islobekabdujabborov@gmail.com>
2026-08-11 15:22:22 +0500

    first commit

--- saxovat/settings.py
+++ saxovat/settings.py
@@ -79,7 +79,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
